5cecb04b20d9093e79a0307a63fdc7e61068b7ee
blackmanppt <blackmanppt@gmail.com>
2016-12-09 16:26:11 +0800

    20161209

---
+++ MVC/Controllers/MessageController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//---新增
+using MVC.Models;
+using MVC.Service;
+
+namespace MVC.Controllers
+{
+    public class MessageController : Controller
+    {
+        messageDBService data = new messageDBService();
+        // GET: Message
+        public ActionResult Index()
+        {
+            //---新增
+            return View(data.DBGetDate());
+        }
+
+        //---新增
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(string status,string ord_nbr)
+        {
+            data.DBCreate(status, ord_nbr);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC/Service/messageDBService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//---新增
+using MVC.Models;
+using MVC.Service;
+
+namespace MVC.Service
+{
+    public class messageDBService
+    {
+        //實做資料庫模型
+        public MVC.Models.TESTEntities db = new Models.TESTEntities();
+
+        //此方法取得資料庫資料回傳
+        public List<Models.ordc_bah1> DBGetDate()
+        {
+            return db.ordc_bah1.ToList();
+        }
+        public void DBCreate(string status,string ord_nbr)
+        {
+            Models.ordc_bah1 NewDate = new Models.ordc_bah1();
+            NewDate.bah_status = status;
+            NewDate.ord_nbr = ord_nbr;
+            NewDate.create_date = DateTime.Now;
+            db.ordc_bah1.Add(NewDate);
+            db.SaveChanges();
+        }
+    }
+}
